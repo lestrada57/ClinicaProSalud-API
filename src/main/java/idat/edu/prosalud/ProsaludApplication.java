b554e24ec93a8b2472c43df7c8bc7dcfd78eb48f
PieroH-del <pharryson1@alumno.uned.es>
2026-02-20 00:39:17 -0500

    first commit

---
+++ src/main/java/idat/edu/prosalud/ProsaludApplication.java
@@ -0,0 +1,13 @@
+package idat.edu.prosalud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsaludApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProsaludApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idat/edu/prosalud/ProsaludApplicationTests.java
@@ -0,0 +1,13 @@
+package idat.edu.prosalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProsaludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
