11d13d7ccbf898a082de07790ff0f4a39722e34e
PieroH-del <pharryson1@alumno.uned.es>
2026-02-27 01:20:36 -0500

    refactor: remove comment from role field in RegisterRequest

--- src/main/java/idat/edu/prosalud/dto/RegisterRequest.java
+++ src/main/java/idat/edu/prosalud/dto/RegisterRequest.java
@@ -7,6 +7,6 @@ public class RegisterRequest {
     private String username;
     private String password;
     private String email;
-    private String role; // ADMIN, MEDICO, RECEPCIONISTA, PACIENTE
+    private String role;
 }
 
